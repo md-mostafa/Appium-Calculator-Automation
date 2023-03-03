2873c78973dbd842e402aec699e3248c6cf742a3
akash <md.mostafa.akash@gmail.com>
2023-03-03 20:09:03 +0600

    basic calculator

---
+++ src/test/java/screens/CalScreen.java
@@ -0,0 +1,87 @@
+package screens;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static setup.Setup.PACKAGE_ID;
+
+public class CalScreen {
+
+    @FindBy(id = PACKAGE_ID + ":id/digit_0")
+    WebElement btn0;
+    @FindBy(id = PACKAGE_ID + ":id/digit_1")
+    WebElement btn1;
+    @FindBy(id = PACKAGE_ID + ":id/digit_2")
+    WebElement btn2;
+    @FindBy(id = PACKAGE_ID + ":id/digit_3")
+    WebElement btn3;
+    @FindBy(id = PACKAGE_ID + ":id/digit_4")
+    WebElement btn4;
+    @FindBy(id = PACKAGE_ID + ":id/digit_5")
+    WebElement btn5;
+    @FindBy(id = PACKAGE_ID + ":id/digit_6")
+    WebElement btn6;
+    @FindBy(id = PACKAGE_ID + ":id/digit_7")
+    WebElement btn7;
+    @FindBy(id = PACKAGE_ID + ":id/digit_8")
+    WebElement btn8;
+    @FindBy(id = PACKAGE_ID + ":id/digit_9")
+    WebElement btn9;
+
+    @FindBy(id = PACKAGE_ID + ":id/op_add")
+    WebElement btnPlus;
+    @FindBy(id = PACKAGE_ID + ":id/op_sub")
+    WebElement btnMinus;
+    @FindBy(id = PACKAGE_ID + ":id/op_mul")
+    WebElement btnMul;
+    @FindBy(id = PACKAGE_ID + ":id/op_div")
+    WebElement btnDiv;
+
+    @FindBy(id = PACKAGE_ID + ":id/result_final")
+    WebElement resultFinal;
+    @FindBy(id = PACKAGE_ID + ":id/eq")
+    WebElement btnEql;
+    @FindBy(id = PACKAGE_ID + ":id/clr")
+    public WebElement btnClear;
+
+    public CalScreen(AndroidDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public String doSum() {
+        btn9.click();
+        btnPlus.click();
+        btn5.click();
+        btnEql.click();
+        return resultFinal.getText();
+    }
+
+    public String doSub() {
+        btn8.click();
+        btnMinus.click();
+        btn3.click();
+        btnEql.click();
+        return resultFinal.getText();
+    }
+
+    public String doMul() {
+        btn1.click();
+        btn4.click();
+        btnMul.click();
+        btn5.click();
+        btnEql.click();
+        return resultFinal.getText();
+    }
+
+    public String doDiv() {
+        btn7.click();
+        btn0.click();
+        btnDiv.click();
+        btn1.click();
+        btn0.click();
+        btnEql.click();
+        return resultFinal.getText();
+    }
+}
---
+++ src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,48 @@
+package testRunner;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import screens.CalScreen;
+import setup.Setup;
+
+public class TestRunner extends Setup {
+
+    @Test(priority = 1, description = "Sum of two numbers")
+    public void doSum() {
+        CalScreen calScreen = new CalScreen(driver);
+        String result = calScreen.doSum();
+        System.out.println("Sum = "+result);
+        Assert.assertEquals(result, "14", "Sum calculation mismatched");
+    }
+
+    @Test(priority = 2, description = "Substraction of two numbers")
+    public void doSub() {
+        CalScreen calScreen = new CalScreen(driver);
+        String result = calScreen.doSub();
+        System.out.println("Sum = "+result);
+        Assert.assertEquals(result, "5", "Substraction calculation mismatched");
+    }
+
+    @Test(priority = 3, description = "Multiplication of two numbers")
+    public void doMul(){
+        CalScreen calScreen = new CalScreen(driver);
+        String result = calScreen.doMul();
+        System.out.println("Mul = "+result);
+        Assert.assertEquals(result, "70",  "Multiplication calculation mismatched");
+    }
+
+    @Test(priority = 4, description = "Division of two numbers")
+    public void doDiv(){
+        CalScreen calSreen = new CalScreen(driver);
+        String result = calSreen.doDiv();
+        System.out.println("Div = "+result);
+        Assert.assertEquals(result,"7", "Division calculation mismatched");
+    }
+
+    @AfterMethod
+    public void clearScreen(){
+        CalScreen calScreen = new CalScreen(driver);
+        calScreen.btnClear.click();
+    }
+}
